4fb848f21dd08f78a6460985151ed6e6690924a0
Vera Egorova <nord.west.2007@yandex.ru>
2024-10-12 18:15:16 +0300

    Добавлена параметризация , тесты на get_price и get_reciept

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,17 +1,18 @@
 from praktikum.bun import Bun
-import unittest
 
 
-class TestBuns(unittest.TestCase):
+class TestBuns:
 
     def test_get_name(self):
         name = 'black bun'
         price = 150
         bun = Bun(name, price)
+
         assert bun.get_name() == name
 
     def test_get_price(self):
         name = 'black bun'
         price = 150
         bun = Bun(name, price)
+
         assert bun.get_price() == price
